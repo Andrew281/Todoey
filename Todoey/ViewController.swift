8a7f99011f5bc3af5943e2d026dd38d2da48b65c
Андрей Чеканников <andreychekannikov@macbook-air-andrej.local>
2019-03-14 10:11:00 +0300

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Андрей Чеканников on 14/03/2019.
+//  Copyright © 2019 Andrey Chekannikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
