3c8455028722a56822036bf5832eb83f3fe47bb4
Андрей Чеканников <andreychekannikov@macbook-air-andrej.local>
2019-03-18 11:14:33 +0300

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Андрей Чеканников on 15/03/2019.
+//  Copyright © 2019 Andrey Chekannikov. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
