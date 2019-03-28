407159cdf489a193653e2e99c88352bd9c5170d3
Андрей Чеканников <andrew281@rambler.ru>
2019-03-28 10:39:49 +0300

    Prepare warnings clearing

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -8,20 +8,34 @@
 
 import UIKit
 import CoreData
+import RealmSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-    
+        print(Realm.Configuration.defaultConfiguration.fileURL)
+        
+        let data = Data()
+        data.name = "Angela"
+        data.age = 12
+        
+        do {
+            let realm = try Realm()
+            try realm.write {
+                realm.add(data)
+            }
+        } catch {
+            print("Error initialising new realm, \(error)")
+        }
         
         return true
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
         
         self.saveContext()
@@ -30,7 +44,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: - Core Data stack
     
     lazy var persistentContainer: NSPersistentContainer = {
-
+        
         let container = NSPersistentContainer(name: "DataModel")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
